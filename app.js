7e45c56fe458f5841f70c136ffffd59da16ca98a
Aram Hekimian <aramhek121@gmail.com>
2023-02-27 17:02:58 -0600

    Change to async/await

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
+// eslint-disable-next-line func-names
 (function () {
   const gif = document.querySelector("img");
   const btn = document.querySelector("button");
   const search = document.querySelector("input");
 
-  function getGif() {
-    let link =
-      "https://api.giphy.com/v1/gifs/translate?api_key=y60pTrIDVUTcG99qteqa4CT5DhrJDpRp&s=dogs";
-    if (search.value !== "") {
-      link = `https://api.giphy.com/v1/gifs/translate?api_key=y60pTrIDVUTcG99qteqa4CT5DhrJDpRp&s=${search.value}`;
+  async function getGif() {
+    try {
+      let link =
+        "https://api.giphy.com/v1/gifs/translate?api_key=y60pTrIDVUTcG99qteqa4CT5DhrJDpRp&s=dogs";
+      if (search.value !== "") {
+        link = `https://api.giphy.com/v1/gifs/translate?api_key=y60pTrIDVUTcG99qteqa4CT5DhrJDpRp&s=${search.value}`;
+      }
+      const response = await fetch(link, { mode: "cors" });
+      const gifData = await response.json();
+      gif.src = gifData.data.images.original.url;
+    } catch (error) {
+      gif.src = "./error.gif";
     }
-    fetch(link, { mode: "cors" })
-      .then((response) => response.json())
-      .then((data) => {
-        gif.src = data.data.images.original.url;
-      })
-      .catch((err) => {
-        gif.src = "./error.gif";
-        console.log(err);
-      });
   }
   getGif();
   btn.addEventListener("click", getGif);
